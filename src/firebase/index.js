9d71f6d9a0b3c4df5b6d7d0c129e47930b103d8b
Ahmed Tirhmert <ahmedtoto301@gmail.com>
2021-10-28 09:40:50 +0100

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,13 +4,7 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyDPbsE0YOca9cDdezjY49Mgq8A9Vbxgng4",
-  authDomain: "todoapp-63f53.firebaseapp.com",
-  projectId: "todoapp-63f53",
-  storageBucket: "todoapp-63f53.appspot.com",
-  messagingSenderId: "790111046144",
-  appId: "1:790111046144:web:1138573f758c57da31a6e5",
-  measurementId: "G-Z4T3ELS0R8",
+      //FIREBASE CONFIGURATION OBJECT
 };
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
